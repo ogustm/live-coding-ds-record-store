20dc137d4ade5600b707dfc5f7eef88abca5ad95
Vasileios Psychas <vaspsych@gmail.com>
2019-11-21 13:05:35 +0100

    Express validator v1

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const { body } = require('express-validator');
 
 /** ROUTERS */
 const indexRouter = require('./routes/index');
@@ -46,9 +47,24 @@ app.use(setCors);
 /** STATIC FILES */
 app.use(express.static(path.join(__dirname, 'public')));
 
+const validationRules = () => {
+  return [
+    body('email')
+      .isEmail()
+      .exists()
+      .normalizeEmail()
+      .withMessage('Do you call this an email?'),
+    body('password')
+      .isLength({ min: 10 })
+      .withMessage('Your password should be 10 characters long.'),
+    body('firstName').trim(),
+    body('lastName').trim()
+  ];
+};
+
 /** ROUTES */
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', validationRules(), usersRouter);
 app.use('/records', recordsRouter);
 app.use('/orders', ordersRouter);
 
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const createError = require('http-errors');
+const { validationResult } = require('express-validator');
 
 exports.getUsers = async (req, res, next) => {
   try {
@@ -44,6 +45,12 @@ exports.updateUser = async (req, res, next) => {
 
 exports.addUser = async (req, res, next) => {
   try {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
     const user = new User(req.body);
     await user.save();
     res.status(200).send(user);
