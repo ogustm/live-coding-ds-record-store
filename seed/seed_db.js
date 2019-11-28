140434e14c68a280f21bc1bede844dfedc62fd1f
Vasileios Psychas <vaspsych@gmail.com>
2019-11-28 09:47:34 +0100

    Seed script Updated

--- seed/seed_db.js
+++ seed/seed_db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const faker = require('faker');
 const User = require('../models/User');
+const Record = require('../models/Record');
+const Order = require('../models/Order');
 
 (async function() {
   /** CONNECT TO MONGO */
@@ -21,6 +23,7 @@ const User = require('../models/User');
 
   console.log(`First, i will delete all the old users`);
 
+  /** DELETE ALL USERS */
   try {
     await User.deleteMany({});
     console.log('Old users moved to a better place. Spandau');
@@ -28,8 +31,24 @@ const User = require('../models/User');
     console.log(e);
   }
 
+  /** DELETE ALL RECORDS */
+  try {
+    await Record.deleteMany({});
+    console.log('Old records moved to a better place. Spandau');
+  } catch (e) {
+    console.log(e);
+  }
+
+  /** DELETE ALL ORDERS */
+  try {
+    await Order.deleteMany({});
+    console.log('Old orders moved to a better place. Spandau');
+  } catch (e) {
+    console.log(e);
+  }
   console.log(`I am creating 20 fake users`);
 
+  /** CREATE 20 FAKE USERS */
   const userPromises = Array(20)
     .fill(null)
     .map(() => {
@@ -39,7 +58,11 @@ const User = require('../models/User');
         email: faker.internet.email(),
         password: faker.internet.password(),
         birthday: faker.date.past(),
-        userName: faker.internet.userName()
+        userName: faker.internet.userName(),
+        address: {
+          city: faker.address.city(),
+          street: faker.address.streetName()
+        }
       });
 
       return user.save();
@@ -48,6 +71,29 @@ const User = require('../models/User');
   try {
     await Promise.all(userPromises);
     console.log('Users stored in the database!');
+  } catch (e) {
+    console.log(e);
+  }
+
+  console.log(`I am creating 20 fake records`);
+
+  /** CREATE 20 FAKE RECORDS */
+  const recordPromises = Array(20)
+    .fill(null)
+    .map(() => {
+      const record = new Record({
+        title: faker.random.words(),
+        artist: faker.internet.userName(),
+        year: new Date(faker.date.past()).getFullYear(),
+        price: faker.finance.amount()
+      });
+
+      return record.save();
+    });
+
+  try {
+    await Promise.all(recordPromises);
+    console.log('Records stored in the database!');
   } catch (e) {
     console.log(e);
   }
